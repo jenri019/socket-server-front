1a1d8d62a22f449713ec0ea2f9c821ddf393a93d
jenri019 <j.enrique209436@hotmail.com>
2025-06-06 10:10:12 -0600

    feat: adding prop to socket, adding service to emit messages

--- src/app/shared/services/websocket.service.ts
+++ src/app/shared/services/websocket.service.ts
@@ -1,25 +1,34 @@
-import { effect, inject, Injectable, signal } from '@angular/core';
-import { io } from "socket.io-client";
+import { Injectable, signal } from '@angular/core';
+import { io, Socket } from "socket.io-client";
 import { environment } from '../../../environments/environment';
 @Injectable({
     providedIn: 'root'
 })
 export class WebsocketService {
+    socket: Socket;
     socketStatus = signal<boolean>(false);
-    constructor() {
-        const socket = io(environment.wsUrl, {
-            transports: ['websocket'] // Forzar el uso de WebSocket
-        });
 
-        socket.on('connect', () => {
+    constructor() {
+        this.socket = io(environment.wsUrl, {
+            transports: ['websocket']
+        });
+        this.checkStatus();
+    }
+
+    checkStatus() {
+        this.socket.on('connect', () => {
             console.log('Socket connected');
             this.socketStatus.set(true);
         });
 
-        socket.on('disconnect', () => {
+        this.socket.on('disconnect', () => {
             console.log('Socket disconnected');
             this.socketStatus.set(false);
         });
     }
 
+    emit(event: string, payload?: any, callback?: Function) {
+        console.log('Emitting mensaje');
+        this.socket.emit(event, payload, callback)
+    }
 }
