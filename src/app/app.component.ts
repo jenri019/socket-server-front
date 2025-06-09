eefd727e1222e0c421de9abc6a43ad76e91a29fe
jenri019 <j.enrique209436@hotmail.com>
2025-06-09 11:00:39 -0600

    feat: adding router to view pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from "./shared/footer/footer.component";
-import { ChatService } from './shared/services/chat.service';
-import { ChatComponent } from "./shared/components/chat/chat.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
     selector: 'app-root',
     imports: [
-        //RouterOutlet,
-        FooterComponent,
-        ChatComponent
+        RouterOutlet,
+        FooterComponent
     ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
