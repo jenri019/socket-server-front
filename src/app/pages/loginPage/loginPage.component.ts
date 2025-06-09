9e3c145e7e13b809c1fef778a907053b840e2684
jenri019 <j.enrique209436@hotmail.com>
2025-06-09 11:02:05 -0600

    feat: adding login page

---
+++ src/app/pages/loginPage/loginPage.component.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { WebsocketService } from '../../services/websocket.service';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-login-page',
+    imports: [
+        ReactiveFormsModule
+    ],
+    templateUrl: './loginPage.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class LoginPageComponent {
+
+    _websocketService = inject(WebsocketService);
+    router = inject(Router);
+
+    private _formBuilder = inject(FormBuilder);
+    form: FormGroup = this._formBuilder.group({
+        username: ['']
+    });
+
+    login() {
+        const username = this.form.get('username')?.value.trim();
+        if (!username || username === '') return;
+        this._websocketService.loginWs(username)
+            .then(() => {
+                console.log('Login successful');
+                this.form.reset();
+                this.router.navigate(['/messages']);
+            })
+            .catch(error => {
+                console.error('Login error:', error);
+            });
+    }
+}
